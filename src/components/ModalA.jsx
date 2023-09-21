9b0dc59a2467402f2b606990c9fec35b254e8b9f
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2023-09-22 05:40:18 +0600

    implemented Modal A, B and C functionality

--- src/components/ModalA.jsx
+++ src/components/ModalA.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import toast from 'react-hot-toast';
-import './Problem-2.css';
 import BangladeshFlag from '../images/Bangladesh.jpg';
 import USAFlag from '../images/USA.png';
-import { Link, NavLink } from 'react-router-dom';
+import './Problem-2.css';
+import { Link } from 'react-router-dom';
 
 
 
 
-const Problem2 = () => {
+
+
+
+
+const ModalA = () => {
+    const [showModal, setShowModal] = useState(true);
+    const handleClose = () => setShowModal(false);
 
 
 
@@ -20,6 +24,13 @@ const Problem2 = () => {
 
 
     const [allContacts, setAllContacts] = useState([])
+
+    useEffect(() => {
+        fetch('https://contact.mediusware.com/api/contacts/')
+            .then(res => res.json())
+            .then(data => setAllContacts(data.results))
+    }, [])
+
     const handleAllContactsClick = () => {
         setModalShow(true);
         setModalShow1(false);
@@ -63,6 +74,8 @@ const Problem2 = () => {
 
 
 
+
+    const [modalShowC, setModalShowC] = useState(false);
     const [modalCData, setModalCData] = useState(null)
     const handleOpenModalC = (data) => {
         console.log("Data: ", data);
@@ -71,168 +84,6 @@ const Problem2 = () => {
         // setModalShow1(false)
         setModalShowC(true);
     }
-
-
-
-
-
-
-
-
-
-
-
-    const [modalShow, setModalShow] = useState(false);
-    const [modalShow1, setModalShow1] = useState(false);
-    const [modalShowC, setModalShowC] = useState(false);
-    function MyVerticallyCenteredModal(props) {
-        return (
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        <p >Modal A</p>
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-
-                    {
-                        checked === true ?
-                            <>
-                                {
-                                    filterData?.map((data, index) => (
-                                        <div className='d-flex  gap-3' key={index}>
-                                            <p className=''>⦿ <span className='fw-bold'>Country:</span> {data?.country.name}</p>
-                                            <p>⦿ <span className='fw-bold'>Phone:</span> {data?.phone}</p>
-                                            <p>⦿ <span className='fw-bold'>ID:</span> {data?.country.id}</p>
-                                        </div>
-                                    ))
-                                }
-                            </>
-
-                            :
-                            <>
-                                {
-                                    allContacts?.map((data, index) => (
-                                        <div onClick={() => handleOpenModalC(data)} className='d-flex  gap-3 bg-color-on-hover' key={index}>
-
-                                            {
-                                                data?.country.name === 'United States'
-                                                    ?
-                                                    <>
-                                                        <p className=''>⦿ <span className={`fw-bold `}>Country:</span> <span className={`fw-bold text-primary`}>{data?.country.name}</span></p>
-                                                        <p>⦿ <span className='fw-bold'>Phone:</span> <span className={`fw-bold text-primary`}>{data?.phone}</span></p>
-                                                    </>
-                                                    :
-                                                    <>
-                                                        {
-                                                            data?.country.name === 'Bangladesh' ?
-                                                                <>
-                                                                    <p className=''>⦿ <span className={`fw-bold `}>Country:</span> <span className={`fw-bold text-success`}>{data?.country.name}</span></p>
-                                                                    <p>⦿ <span className='fw-bold'>Phone:</span> <span className={`fw-bold text-success`}>{data?.phone}</span></p>
-                                                                </>
-                                                                :
-                                                                <>
-                                                                    <p className=''>⦿ <span className={`fw-bold `}>Country:</span> <span className={`fw-bold text-dark`}>{data?.country.name}</span></p>
-                                                                    <p>⦿ <span className='fw-bold'>Phone:</span> <span className={`fw-bold text-dark`}>{data?.phone}</span></p>
-                                                                </>
-                                                        }
-                                                    </>
-                                            }
-
-                                        </div>
-                                    ))
-                                }
-
-                            </>
-
-
-
-
-                    }
-
-                    <div className="d-flex justify-content-center mt-5 gap-3">
-                        <button className="btn btn-lg btn-outline-primary" type="button">All Contacts</button>
-                        <button className="btn btn-lg btn-outline-success" type="button" onClick={handleUSContactsClick}>US Contacts</button>
-                        <button className="btn btn-lg btn-danger" type="button" onClick={props.onHide} >Close</button>
-                    </div>
-                </Modal.Body>
-
-                <div className='container'>
-                    <div className="row justify-content-center">
-                        <div className='col-md-6 mx-auto '>
-                            <div className="input-group mt-3 ml-6  mb-5">
-                                <div className="border border-success p-2 input-group-text">
-                                    <input id="checkbox"
-                                        checked={checked}
-                                        onChange={handleCheckBoxClicked}
-                                        className="form-check-input mt-0 border border-dark p-2"
-                                        type="checkbox"
-                                        value=""
-                                        aria-label="Checkbox for following text input"
-                                    />
-                                </div>
-                                <input type="text" className="form-control border border-success p-2" aria-label="Text input with checkbox" placeholder='Only Even'></input>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </Modal>
-        );
-    }
-
-
-
-
-
-
-
-    function MyVerticallyCenteredModal1(props) {
-        return (
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        <p >Modal B</p>
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-
-
-                    {
-                        USContacts?.map((data, index) => (
-                            <div onClick={() => handleOpenModalC(data)} className='d-flex  gap-3 bg-color-on-hover' key={index}>
-                                <p className=''>⦿ <span className='fw-bold'>Country:</span> <span className='fw-bold text-primary'>{data?.country.name}</span></p>
-                                <p>⦿ <span className='fw-bold'>Phone:</span> <span className='fw-bold text-primary'>{data?.phone}</span></p>
-                            </div>
-                        ))
-                    }
-
-
-                    <div className="d-flex justify-content-center mt-5 gap-3 mb-5">
-                        <button className="btn btn-lg btn-outline-primary" type="button" onClick={handleAllContactsClick}>All Contacts</button>
-                        <button className="btn btn-lg btn-outline-success" type="button" >US Contacts</button>
-                        <button className="btn btn-lg btn-danger" type="button" onClick={props.onHide} >Close</button>
-                    </div>
-                </Modal.Body>
-            </Modal>
-        );
-    }
-
-
-
-
-
-
 
 
     function MyVerticallyCenteredModalC(props) {
@@ -292,78 +143,114 @@ const Problem2 = () => {
 
 
 
-
-
-
-
-
-
     return (
-
         <div>
-            <div className="container">
-                <div className="row justify-content-center mt-5">
-                    <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
+            <h1 className='text-center mt-5 mb-5'>Modal A</h1>
 
-                    <div className="d-flex justify-content-center gap-3">
+            <Modal size="lg" show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal A</Modal.Title>
+                </Modal.Header>
 
-                        <Link to='/problem-2/modalA'  onClick={handleAllContactsClick}>
-                            <button  className="btn btn-lg btn-outline-primary" type="button">All Contacts</button>
-                        </Link>
+                <Modal.Body>
+                    {
+                        checked === true ?
+                            <>
+                                {
+                                    filterData?.map((data, index) => (
+                                        <div onClick={() => handleOpenModalC(data)} className='d-flex  gap-3 bg-color-on-hover' key={index}>
+                                            <p className=''>⦿ <span className={`fw-bold `}>Country:</span> <span className={`fw-bold text-primary`}>{data?.country.name}</span></p>
+                                            <p>⦿ <span className='fw-bold'>Phone:</span> <span className={`fw-bold text-primary`}>{data?.phone}</span></p>
+                                            <p>⦿ <span className='fw-bold'>ID:</span> <span className={`fw-bold text-primary`}>{data?.country.id}</span></p>
+                                        </div>
+                                    ))
+                                }
+                            </>
+
+                            :
+                            <>
+                                {
+                                    allContacts?.map((data, index) => (
+                                        <div onClick={() => handleOpenModalC(data)} className='d-flex  gap-3 bg-color-on-hover' key={index}>
+
+                                            {
+                                                data?.country.name === 'United States'
+                                                    ?
+                                                    <>
+                                                        <p className=''>⦿ <span className={`fw-bold `}>Country:</span> <span className={`fw-bold text-primary`}>{data?.country.name}</span></p>
+                                                        <p>⦿ <span className='fw-bold'>Phone:</span> <span className={`fw-bold text-primary`}>{data?.phone}</span></p>
+                                                    </>
+                                                    :
+                                                    <>
+                                                        {
+                                                            data?.country.name === 'Bangladesh' ?
+                                                                <>
+                                                                    <p className=''>⦿ <span className={`fw-bold `}>Country:</span> <span className={`fw-bold text-success`}>{data?.country.name}</span></p>
+                                                                    <p>⦿ <span className='fw-bold'>Phone:</span> <span className={`fw-bold text-success`}>{data?.phone}</span></p>
+                                                                </>
+                                                                :
+                                                                <>
+                                                                    <p className=''>⦿ <span className={`fw-bold `}>Country:</span> <span className={`fw-bold text-dark`}>{data?.country.name}</span></p>
+                                                                    <p>⦿ <span className='fw-bold'>Phone:</span> <span className={`fw-bold text-dark`}>{data?.phone}</span></p>
+                                                                </>
+                                                        }
+                                                    </>
+                                            }
+
+                                        </div>
+                                    ))
+                                }
+
+                            </>
+
+
+
+
+                    }
+
+                    <div className="d-flex justify-content-center mt-5 gap-3">
+                        <button className="btn btn-lg btn-outline-primary" type="button">All Contacts</button>
 
                         <Link to='/problem-2/modalB'>
-                            <button className="btn btn-lg btn-outline-warning" type="button" onClick={handleUSContactsClick}>US Contacts</button>
+                            <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
                         </Link>
-
+                        
+                        <button className="btn btn-lg btn-danger" type="button" onClick={handleClose} >Close</button>
                     </div>
+                </Modal.Body>
 
+
+                <div className='container'>
+                    <div className="row justify-content-center">
+                        <div className='col-md-6 mx-auto '>
+                            <div className="input-group mt-3 ml-6  mb-5">
+                                <div className="border border-success p-2 input-group-text">
+                                    <input id="checkbox"
+                                        checked={checked}
+                                        onChange={handleCheckBoxClicked}
+                                        className="form-check-input mt-0 border border-dark p-2"
+                                        type="checkbox"
+                                        value=""
+                                        aria-label="Checkbox for following text input"
+                                    />
+                                </div>
+                                <input type="text" className="form-control border border-success p-2" aria-label="Text input with checkbox" placeholder='Only Even'></input>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
+            </Modal>
 
 
-
-
-
-            <MyVerticallyCenteredModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
-
-
-            <MyVerticallyCenteredModal1
-                show={modalShow1}
-                onHide={() => setModalShow1(false)}
-            />
 
             <MyVerticallyCenteredModalC
                 show={modalShowC}
                 onHide={() => setModalShowC(false)}
             />
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </div>
     );
 };
 
-export default Problem2;
+export default ModalA;
---
+++ src/components/ModalB.jsx
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import BangladeshFlag from '../images/Bangladesh.jpg';
+import USAFlag from '../images/USA.png';
+import './Problem-2.css';
+import { Link } from 'react-router-dom';
+
+
+
+
+
+const ModalB = () => {
+
+    const [showModal, setShowModal] = useState(true);
+    const handleClose = () => setShowModal(false);
+
+
+
+    const [USContacts, setUSContacts] = useState([])
+    useEffect(() => {
+        fetch('https://contact.mediusware.com/api/country-contacts/United States/')
+            .then(res => res.json())
+            .then(data => setUSContacts(data.results))
+    }, [])
+
+
+    const [allContacts, setAllContacts] = useState([])
+    const handleAllContactsClick = () => {
+        setModalShow(true);
+        setModalShow1(false);
+        setModalShowC(false);
+        fetch('https://contact.mediusware.com/api/contacts/')
+            .then(res => res.json())
+            .then(data => setAllContacts(data.results))
+        setChecked(false);
+    }
+
+
+
+    const [modalShowC, setModalShowC] = useState(false);
+    const [modalCData, setModalCData] = useState(null)
+    const handleOpenModalC = (data) => {
+        console.log("Data: ", data);
+        setModalCData(data);
+        setModalShowC(true);
+    }
+
+
+
+
+    function MyVerticallyCenteredModalC(props) {
+        return (
+            <Modal
+                {...props}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <p >Modal C</p>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                    <div>
+                        <p><span className='fw-bold'>Country Name:</span> {modalCData?.country.name}</p>
+                        <p><span className='fw-bold'>Country ID:</span> {modalCData?.country.id}</p>
+                        <p><span className='fw-bold'>Phone Number:</span> {modalCData?.phone}</p>
+                        {
+                            modalCData?.country.name === 'Bangladesh' &&
+                            <div>
+                                <img className='flag-width mx-auto' src={BangladeshFlag} alt="Flag" />
+                            </div>
+                        }
+
+                        {
+                            modalCData?.country.name === 'United States' &&
+                            <div>
+                                <img className='flag-width mx-auto' src={USAFlag} alt="Flag" />
+                            </div>
+                        }
+
+                    </div>
+
+                </Modal.Body>
+
+
+
+            </Modal>
+        );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <div>
+            <h1 className='text-center my-5'>Modal B</h1>
+
+
+            <Modal size="lg" show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal B</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    {
+                        USContacts?.map((data, index) => (
+                            <div onClick={() => handleOpenModalC(data)} className='d-flex  gap-3 bg-color-on-hover' key={index}>
+                                <p className=''>⦿ <span className='fw-bold'>Country:</span> <span className='fw-bold text-primary'>{data?.country.name}</span></p>
+                                <p>⦿ <span className='fw-bold'>Phone:</span> <span className='fw-bold text-primary'>{data?.phone}</span></p>
+                            </div>
+                        ))
+                    }
+
+
+                    <div className="d-flex justify-content-center mt-5 gap-3 mb-5">
+                        
+                        <Link to='/problem-2/modalA'  onClick={handleAllContactsClick}>
+                            <button  className="btn btn-lg btn-outline-primary" type="button">All Contacts</button>
+                        </Link>
+
+                        <button className="btn btn-lg btn-outline-success" type="button" >US Contacts</button>
+                        <button className="btn btn-lg btn-danger" type="button" onClick={handleClose} >Close</button>
+                    </div>
+                </Modal.Body>
+
+
+            </Modal>
+
+
+
+            
+            <MyVerticallyCenteredModalC
+                show={modalShowC}
+                onHide={() => setModalShowC(false)}
+            />
+
+
+
+        </div>
+    );
+};
+
+export default ModalB;
