aeecb07b13758b32af8bc139abac7349aa197823
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2023-09-22 02:49:35 +0600

    update

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import toast from 'react-hot-toast';
 
 
 
@@ -11,9 +12,59 @@ const Problem2 = () => {
 
 
 
+
+
+
+
+    const [allContacts, setAllContacts] = useState([])
+    const handleAllContactsClick = () => {
+        setModalShow(true);
+        fetch('https://contact.mediusware.com/api/contacts/')
+            .then(res => res.json())
+            .then(data => setAllContacts(data.results))
+        setChecked(false);
+        console.log("Check Value Fetch Function: ", checked);
+    }
+
+
+    const handleUSContactsClick = () => {
+        setModalShow1(true)
+    }
+
+
+
+
+
+
+    const [checked, setChecked] = useState(false);
+    const [filterData, setFilterData] = useState([])
+    const handleCheckBoxClicked = () => {
+        setChecked((prevChecked) => !prevChecked);
+        if (checked === true) {
+            const newFilter = allContacts?.length > 0 && allContacts.filter(data => data.country.id === 2 || data.country.id === 4 || data.country.id === 6 || data.country.id === 8 || data.country.id === 10 || data.country.id === 12 || data.country.id === 14 || data.country.id === 16 || data.country.id === 18 || data.country.id === 20 || data.country.id === 22 || data.country.id === 24 || data.country.id === 26 || data.country.id === 28 || data.country.id === 30)
+            setFilterData(newFilter);
+        }
+        else {
+            // toast.error("Not Checked")
+            console.log("Not Checked");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     const [modalShow, setModalShow] = useState(false);
     const [modalShow1, setModalShow1] = useState(false);
-
     function MyVerticallyCenteredModal(props) {
         return (
             <Modal
@@ -28,28 +79,62 @@ const Problem2 = () => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <div className="d-flex justify-content-center gap-3">
+
+                    {
+                        checked === true ?
+                            <>
+                                {
+                                    filterData?.map((data, index) => (
+                                        <div className='d-flex  gap-3' key={index}>
+                                            <p className=''>⦿ <span className='fw-bold'>Country:</span> {data?.country.name}</p>
+                                            <p>⦿ <span className='fw-bold'>Phone:</span> {data?.phone}</p>
+                                            <p>⦿ <span className='fw-bold'>ID:</span> {data?.country.id}</p>
+                                        </div>
+                                    ))
+                                }
+                            </>
+
+                            :
+                            <>
+                                {
+                                    allContacts?.map((data, index) => (
+                                        <div className='d-flex  gap-3' key={index}>
+                                            <p className=''>⦿ <span className='fw-bold'>Country:</span> {data?.country.name}</p>
+                                            <p>⦿ <span className='fw-bold'>Phone:</span> {data?.phone}</p>
+                                        </div>
+                                    ))
+                                }
+                            </>
+
+                    }
+
+                    <div className="d-flex justify-content-center mt-5 gap-3">
                         <button className="btn btn-lg btn-outline-primary" type="button">All Contacts</button>
                         <button className="btn btn-lg btn-outline-success" type="button" onClick={() => setModalShow1(true)}>US Contacts</button>
                         <button className="btn btn-lg btn-danger" type="button" onClick={props.onHide} >Close</button>
                     </div>
                 </Modal.Body>
-                <Modal.Footer>
-                    {/* <Button onClick={props.onHide}>Close</Button> */}
 
-                </Modal.Footer>
-
-                <Form className=" px-6 mb-3 float-left">
-                    {['checkbox'].map((type) => (
-                        <div key={`default-${type}`}>
-                            <Form.Check
-                                type={type}
-                                id={`default-${type}`}
-                                label={`default ${type}`}
-                            />
+                <div className='container'>
+                    <div className="row justify-content-center">
+                        <div className='col-md-6 mx-auto '>
+                            <div className="input-group mt-3 ml-6  mb-5">
+                                <div className="border border-success p-2 input-group-text">
+                                    <input id="checkbox"
+                                        checked={checked}
+                                        onChange={handleCheckBoxClicked}
+                                        className="form-check-input mt-0 border border-dark p-2"
+                                        type="checkbox"
+                                        value=""
+                                        aria-label="Checkbox for following text input"
+                                    />
+                                </div>
+                                <input type="text" className="form-control border border-success p-2" aria-label="Text input with checkbox" placeholder='Only Even'></input>
+                            </div>
                         </div>
-                    ))}
-                </Form>
+                    </div>
+                </div>
+
             </Modal>
         );
     }
@@ -80,12 +165,20 @@ const Problem2 = () => {
                         <button className="btn btn-lg btn-danger" type="button" onClick={props.onHide} >Close</button>
                     </div>
                 </Modal.Body>
-                <Modal.Footer>
-                    {/* <Button onClick={props.onHide}>Close</Button> */}
-                </Modal.Footer>
             </Modal>
         );
     }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -100,23 +193,11 @@ const Problem2 = () => {
                     <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
 
                     <div className="d-flex justify-content-center gap-3">
-                        <>
-                            <button className="btn btn-lg btn-outline-primary" type="button" onClick={() => setModalShow(true)}>All Contacts</button>
 
-                            <MyVerticallyCenteredModal
-                                show={modalShow}
-                                onHide={() => setModalShow(false)}
-                            />
-                        </>
+                        <button className="btn btn-lg btn-outline-primary" type="button" onClick={handleAllContactsClick}>All Contacts</button>
 
-                        <>
-                            <button className="btn btn-lg btn-outline-warning" type="button" onClick={() => setModalShow1(true)}>US Contacts</button>
+                        <button className="btn btn-lg btn-outline-warning" type="button" onClick={handleUSContactsClick}>US Contacts</button>
 
-                            <MyVerticallyCenteredModal1
-                                show={modalShow1}
-                                onHide={() => setModalShow1(false)}
-                            />
-                        </>
                     </div>
 
                 </div>
@@ -126,11 +207,16 @@ const Problem2 = () => {
 
 
 
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
 
 
-
-
-
+            <MyVerticallyCenteredModal1
+                show={modalShow1}
+                onHide={() => setModalShow1(false)}
+            />
 
 
 
